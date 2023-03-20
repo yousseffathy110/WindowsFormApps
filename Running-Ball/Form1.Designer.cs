f0a3a25b25df455c55074e2e67adbaf8dc278552
yousef fathy <yf1102003@gmail.com>
2023-03-21 00:16:44 +0200

    making the functionality

--- Running-Ball/Form1.Designer.cs
+++ Running-Ball/Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            stick1PictureBox = new PictureBox();
+            stick2PictureBox = new PictureBox();
+            ballPictureBox = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)stick1PictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)stick2PictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ballPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // stick1PictureBox
+            // 
+            stick1PictureBox.BackColor = SystemColors.Highlight;
+            stick1PictureBox.Location = new Point(765, 134);
+            stick1PictureBox.Name = "stick1PictureBox";
+            stick1PictureBox.Size = new Size(23, 139);
+            stick1PictureBox.TabIndex = 0;
+            stick1PictureBox.TabStop = false;
+            // 
+            // stick2PictureBox
+            // 
+            stick2PictureBox.BackColor = SystemColors.Highlight;
+            stick2PictureBox.Location = new Point(12, 134);
+            stick2PictureBox.Name = "stick2PictureBox";
+            stick2PictureBox.Size = new Size(25, 139);
+            stick2PictureBox.TabIndex = 1;
+            stick2PictureBox.TabStop = false;
+            stick2PictureBox.Click += pictureBox2_Click;
+            // 
+            // ballPictureBox
+            // 
+            ballPictureBox.BackColor = Color.FromArgb(128, 255, 128);
+            ballPictureBox.Location = new Point(325, 174);
+            ballPictureBox.Name = "ballPictureBox";
+            ballPictureBox.Size = new Size(85, 60);
+            ballPictureBox.TabIndex = 2;
+            ballPictureBox.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 50;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ballPictureBox);
+            Controls.Add(stick2PictureBox);
+            Controls.Add(stick1PictureBox);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)stick1PictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)stick2PictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ballPictureBox).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox stick1PictureBox;
+        private PictureBox stick2PictureBox;
+        private PictureBox ballPictureBox;
+        private System.Windows.Forms.Timer timer1;
     }
 }
