92a91c37995033da77741f175450179d2a1a8ba2
yousef fathy <yf1102003@gmail.com>
2023-03-20 23:08:01 +0200

    adding the draw and erease functionality to the app

--- drawapp/Form1.Designer.cs
+++ drawapp/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(128, 77);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(451, 288);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
     }
 }
--- drawapp/Form1.cs
+++ drawapp/Form1.cs
@@ -2,9 +2,43 @@ namespace drawapp
 {
     public partial class Form1 : Form
     {
+        private bool isDrawing = false;
+        private Point lastPoint;
+        private Pen pen = new Pen(Color.Black, 2);
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            pictureBox1.MouseUp += pictureBox1_MouseUp;
+           
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = true;
+                lastPoint = e.Location;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing && e.Button == MouseButtons.Left)
+            {
+                Graphics g = pictureBox1.CreateGraphics();
+                g.DrawLine(pen, lastPoint, e.Location);
+                lastPoint = e.Location;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
         }
     }
 }
