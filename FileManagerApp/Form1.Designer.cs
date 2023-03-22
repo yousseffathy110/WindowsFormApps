4d379165ecef48cd82c2066d1ac6685af4b0ece5
yousef fathy <yf1102003@gmail.com>
2023-03-22 02:39:05 +0200

    uploading the project

---
+++ FileManagerApp/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace FileManagerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblPath = new Label();
+            txtPath = new TextBox();
+            btnBrowse = new Button();
+            btnCreateFile = new Button();
+            btnCreateFolder = new Button();
+            btnDelete = new Button();
+            lstFilesAndFolders = new ListBox();
+            SuspendLayout();
+            // 
+            // lblPath
+            // 
+            lblPath.AutoSize = true;
+            lblPath.Location = new Point(49, 45);
+            lblPath.Name = "lblPath";
+            lblPath.Size = new Size(37, 20);
+            lblPath.TabIndex = 0;
+            lblPath.Text = "Path";
+            // 
+            // txtPath
+            // 
+            txtPath.Location = new Point(92, 45);
+            txtPath.Name = "txtPath";
+            txtPath.Size = new Size(295, 27);
+            txtPath.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            btnBrowse.Location = new Point(675, 36);
+            btnBrowse.Name = "btnBrowse";
+            btnBrowse.Size = new Size(94, 29);
+            btnBrowse.TabIndex = 2;
+            btnBrowse.Text = "Browse";
+            btnBrowse.UseVisualStyleBackColor = true;
+            btnBrowse.Click += btnBrowse_Click_1;
+            // 
+            // btnCreateFile
+            // 
+            btnCreateFile.Location = new Point(675, 127);
+            btnCreateFile.Name = "btnCreateFile";
+            btnCreateFile.Size = new Size(94, 29);
+            btnCreateFile.TabIndex = 3;
+            btnCreateFile.Text = "Create File";
+            btnCreateFile.UseVisualStyleBackColor = true;
+            btnCreateFile.Click += btnCreateFile_Click_1;
+            // 
+            // btnCreateFolder
+            // 
+            btnCreateFolder.Font = new Font("Segoe UI", 7.20000029F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCreateFolder.Location = new Point(675, 207);
+            btnCreateFolder.Name = "btnCreateFolder";
+            btnCreateFolder.Size = new Size(94, 29);
+            btnCreateFolder.TabIndex = 4;
+            btnCreateFolder.Text = "CreateFolder";
+            btnCreateFolder.UseVisualStyleBackColor = true;
+            btnCreateFolder.Click += btnCreateFolder_Click_1;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(675, 287);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 5;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click_1;
+            // 
+            // lstFilesAndFolders
+            // 
+            lstFilesAndFolders.FormattingEnabled = true;
+            lstFilesAndFolders.ItemHeight = 20;
+            lstFilesAndFolders.Location = new Point(92, 128);
+            lstFilesAndFolders.Name = "lstFilesAndFolders";
+            lstFilesAndFolders.Size = new Size(295, 264);
+            lstFilesAndFolders.TabIndex = 6;
+            lstFilesAndFolders.SelectedIndexChanged += lstFilesAndFolders_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lstFilesAndFolders);
+            Controls.Add(btnDelete);
+            Controls.Add(btnCreateFolder);
+            Controls.Add(btnCreateFile);
+            Controls.Add(btnBrowse);
+            Controls.Add(txtPath);
+            Controls.Add(lblPath);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblPath;
+        private TextBox txtPath;
+        private Button btnBrowse;
+        private Button btnCreateFile;
+        private Button btnCreateFolder;
+        private Button btnDelete;
+        private ListBox lstFilesAndFolders;
+    }
+}
---
+++ FileManagerApp/Form1.cs
@@ -0,0 +1,103 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+
+
+namespace FileManagerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void LoadFilesAndFolders()
+        {
+            if (string.IsNullOrEmpty(txtPath.Text) || !Directory.Exists(txtPath.Text))
+            {
+                return;
+            }
+
+            lstFilesAndFolders.Items.Clear();
+
+            var dirs = Directory.GetDirectories(txtPath.Text);
+            foreach (string dir in dirs)
+            {
+                lstFilesAndFolders.Items.Add(Path.GetFileName(dir));
+            }
+
+            var files = Directory.GetFiles(txtPath.Text);
+            foreach (string file in files)
+            {
+                lstFilesAndFolders.Items.Add(Path.GetFileName(file));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click_1(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    txtPath.Text = fbd.SelectedPath;
+                    LoadFilesAndFolders();
+                }
+            }
+        }
+
+        private void btnCreateFile_Click_1(object sender, EventArgs e)
+        {
+            string fileName = $"NewFile_{DateTime.Now.ToString("yyyyMMdd_HHmmss")}.txt";
+            string filePath = Path.Combine(txtPath.Text, fileName);
+
+            File.Create(filePath).Close();
+            LoadFilesAndFolders();
+        }
+
+        private void btnCreateFolder_Click_1(object sender, EventArgs e)
+        {
+            string folderName = $"NewFolder_{DateTime.Now.ToString("yyyyMMdd_HHmmss")}";
+            string folderPath = Path.Combine(txtPath.Text, folderName);
+
+            Directory.CreateDirectory(folderPath);
+            LoadFilesAndFolders();
+        }
+
+        private void btnDelete_Click_1(object sender, EventArgs e)
+        {
+            if (lstFilesAndFolders.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a file or folder to delete.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string selectedItem = lstFilesAndFolders.SelectedItem.ToString();
+            string fullPath = Path.Combine(txtPath.Text, selectedItem);
+
+            if (Directory.Exists(fullPath))
+            {
+                Directory.Delete(fullPath, true);
+            }
+            else if (File.Exists(fullPath))
+            {
+                File.Delete(fullPath);
+            }
+
+            LoadFilesAndFolders();
+        }
+
+        private void lstFilesAndFolders_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
